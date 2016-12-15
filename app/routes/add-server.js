35b067abdaaf49d2c8850c7712cf0d3b1f52ab3f
叶枫酒吧工作室 <718063475@qq.com>
2016-12-15 17:47:34 +0800

    some code

--- app/routes/add-server.js
+++ app/routes/add-server.js
@@ -1,9 +1,6 @@
 import Ember from 'ember';
 import layer from 'layer';
 export default Ember.Route.extend({
-  beforeModel(transition) {
-    layer.msg('loading......')
-  },
   model: function () {
     var qwe = 0;
     for (var i = 0; i < 999000000; i++) {
@@ -13,14 +10,8 @@ export default Ember.Route.extend({
   },
   actions: {
     loading(transition, originRoute) {
-      //displayLoadingSpinner();
-      this.router.one('didTransition', function () {
-        // hideLoadingSpinner();
-      });
-      // Return true to bubble this event to `FooRoute`
-      // or `ApplicationRoute`.
+      displayLoadingSpinner();
       return true;
     }
   }
-}
-})
+});
