61bedbfd5ad47d732949d889b028e34318ab3060
yefegnbar <718063475@qq.com>
2016-12-19 00:47:26 +0800

    fixed loading model

--- app/routes/add-server.js
+++ app/routes/add-server.js
@@ -1,12 +1,16 @@
 import Ember from 'ember';
 import layer from 'layer';
-
+import RSVP from 'rsvp';
+import inject from 'ember-service/inject'
+import { later } from 'ember-runloop'
 export default Ember.Route.extend({
-	
+	ajax: inject(),
 	model: function() {
-		return 'qwe';
+		return new RSVP.Promise((resolve, reject) => later(() => reject({
+    		status: 200,
+    		message: 'this is loading test...'
+		}), 5000));
 	},
-	
 	actions: {
 		getGids:function(params){
 //			let gids = this.get('value');
