0fe31f6c35d540fb70ba136306f8ca586fa19cd2
yefengbar <718063475@qq.com>
2016-12-20 19:27:27 +0800

    add game-manager route

--- app/router.js
+++ app/router.js
@@ -3,19 +3,6 @@ import config from './config/environment';
 
 const Router = Ember.Router.extend({
   location: config.locationType,
-  willTransition() {
-    this._super(...arguments
-  )
-    ;
-    performance.mark('willTransition');
-  },
-
-  didTransition() {
-    this._super(...arguments
-  )
-    ;
-    performance.mark('didTransition');
-  },
   rootURL: config.rootURL
 });
 
@@ -25,8 +12,7 @@ Router.map(function() {
   this.route('news');
   this.route('new', {path: 'news/:new_id'});
   this.route('add-server');
-  // this.route('addServer');
-  // this.route('add_server_loading');
+  this.route('game-manager');
 });
 
 export default Router;
