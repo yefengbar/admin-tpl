72e8f68d138902036904380c169852a62a47e0e7
yefengbar <718063475@qq.com>
2016-12-21 12:00:51 +0800

    refactoring datatable

--- app/routes/game-manager.js
+++ app/routes/game-manager.js
@@ -3,7 +3,14 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   afterModel:function (posts, transition) {
     setTimeout(function () {
-      Ember.$('.dataTables').dataTable();
+      let table = Ember.$('.dataTables').DataTable({
+        "lengthChange": true,
+        "lengthMenu": [ [25, 50,100, -1], [ 25, 50,100, "全部"] ],
+        "processing": true,
+        "orderClasses": true
+      });
+      // table.page.len( -1 ).draw();
+      table.page.len( 25 ).draw();
     },100);
 
   }
